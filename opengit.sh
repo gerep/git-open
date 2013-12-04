712f3cf21bf85399ae13d12736790a31e9f91443
Gerep <daniel.gerep@gmail.com>
2013-12-04 18:42:34 -0200

    Changes how the _opengit_open function checks for $MACHTYPE

--- opengit.sh
+++ opengit.sh
@@ -1,7 +1,7 @@
 function _opengit_open()
 {
   local open_cmd='huh?'
-  if [ -n `echo $MACHTYPE | grep darwin` ]; then
+  if [ -z `echo $MACHTYPE | grep linux` ]; then
     open_cmd='open'
   else
     open_cmd='xdg-open'
